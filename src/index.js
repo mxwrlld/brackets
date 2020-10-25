3b9803782f023d3434dc38f925a976ac26adac56
mxwrlld <maksim.mixwell@gmail.com>
2020-10-25 23:06:33 +0500

    init: practically a solution to the balancing problem

--- src/index.js
+++ src/index.js
@@ -1,3 +1,30 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let modifyStr = str.split('');
+  let stack = [];
+  let arr =  bracketsConfig.flat();
+  let flag = true;
+
+  for (let i = 0; i < modifyStr.length; ++i) {
+    let bracketIndex = arr.indexOf(modifyStr[i]);
+
+    if(arr[bracketIndex] == arr[bracketIndex + 1]) {
+      if(flag){
+        flag = false;
+      }
+      else {
+        bracketIndex += 1;
+        flag = true;
+      }
+    }
+
+    if(bracketIndex %2 == 0) {
+      stack.push(bracketIndex + 1);
+    }
+    else {
+        if(stack.pop() != bracketIndex) {
+          return false;
+        }
+    }
+  }
+  return stack.length == 0;
 }
